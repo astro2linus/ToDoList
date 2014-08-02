ef8b94b59208e0c1a39c01361cd6a910623b9f07
astro2linus <astro2linus@gmail.com>
2014-08-03 01:37:43 +0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Linus on 03/08/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Linus on 03/08/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
